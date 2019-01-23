bf6e5963546a2d5073edf5074372e7bf6437eae5
Heather You <heather.fswd@gmail.com>
2019-01-23 16:57:59 -0500

    Spike echo server

---
+++ src/main/java/com/heatheryou/echoserver/EchoServer.java
@@ -0,0 +1,30 @@
+package com.heatheryou.echoserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        int port = 5000;
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(port);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                )
+        {
+            String inputLine;
+            while ((inputLine = input.readLine()) != null) {
+                output.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when listening to port " + port);
+            System.out.println(e.getMessage());
+        }
+    }
+}
